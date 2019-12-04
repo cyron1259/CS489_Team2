11d131ec505f9add552a1377fbe91a74f8bd94f8
Ubuntu <ubuntu@ip-172-31-33-227.us-west-2.compute.internal>
2019-12-04 12:12:55 +0000

    distribution and min bug fix. implemented last API

--- biasexplorer/settings.py
+++ biasexplorer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*+y$x6s9$rkcuw$(+(z_6)sisjc_mlmii^mg$5$b-q(er03&7z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cs489.hyunwoo.me']
 
 
 # Application definition
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
